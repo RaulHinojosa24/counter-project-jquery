139174e6bde93ae896718b0ff587241e3ffe27c7
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-28 09:23:58 +0200

    solucionado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 let counter = 0;
 
 $(document).ready(function () {
-    console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
-
+    $(".prevBtn").click(() => {
+        counter--;
+        updateCounter();
+    });
+    $(".nextBtn").click(() => {
+        counter++;
+        updateCounter();
+    });
 });
+
+function updateCounter() {
+    $(".counter").text(counter);
+    setCounterColor();
+}
+
+function setCounterColor() {
+    if (counter > 0) {
+        $(".counter").removeClass("negativo");
+        $(".counter").addClass("positivo");
+    } else if (counter < 0) {
+        $(".counter").removeClass("positivo");
+        $(".counter").addClass("negativo");
+    } else {
+        $(".counter").removeClass("positivo");
+        $(".counter").removeClass("negativo");
+    }
+}
